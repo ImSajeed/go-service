42495d788955aa70aa4f70fd03bac73d7a235a5e
Sajeed <sk.sajeed9@gmail.com>
2021-08-08 12:41:33 +0530

    endpoints to store and fetch from mongo and inmemory

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	controllers "go-service/Controllers"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -13,7 +14,7 @@ func main() {
 	http.HandleFunc("/in-memory", controllers.ProcessInMemoryRecords)
 
 	fmt.Printf("Starting server...\n")
-	if err := http.ListenAndServe(":8090", nil); err != nil {
+	if err := http.ListenAndServe(os.Getenv("PORT"), nil); err != nil {
 		log.Fatal(err)
 	}
 }
